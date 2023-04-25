5165a01d476878db7da4afaa48d706ce41154412
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2023-04-25 15:20:14 -0700

    Adapt for new menu system

---
+++ src/data/usr/lib/lua/aredn/nav/user/35waterfall.lua
@@ -0,0 +1,4 @@
+local wifi_nr = aredn.hardware.get_iface_name("wifi"):match("wlan(%d+)"):match("wlan(%d+)")
+if wifi_nr and nixio.fs.stat("/sys/kernel/debug/ieee80211/phy" .. wifi_nr .. "/ath9k") then
+    return { href = "waterfall", display = "Waterfall", hint = "Waterfall of wireless activity" }
+end
