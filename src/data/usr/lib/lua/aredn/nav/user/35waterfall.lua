bb76c1ed11320dd3ef79e8f16da3369d4f9544d8
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2023-10-12 10:02:53 -0700

    Prototype waterfall for AC devices

--- src/data/usr/lib/lua/aredn/nav/user/35waterfall.lua
+++ src/data/usr/lib/lua/aredn/nav/user/35waterfall.lua
@@ -1,4 +1,4 @@
 local wifi_nr = aredn.hardware.get_iface_name("wifi"):match("wlan(%d+)")
-if wifi_nr and nixio.fs.stat("/sys/kernel/debug/ieee80211/phy" .. wifi_nr .. "/ath9k") then
+if wifi_nr and (nixio.fs.stat("/sys/kernel/debug/ieee80211/phy" .. wifi_nr .. "/ath9k") or nixio.fs.stat("/sys/kernel/debug/ieee80211/phy" .. wifi_nr .. "/ath10k")) then
     return { href = "waterfall", display = "Waterfall", hint = "Waterfall of wireless activity" }
 end
